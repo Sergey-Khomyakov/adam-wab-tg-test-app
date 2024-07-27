97b3272d3268f6407a2bbfeff08bac34ed0dacae
Sergey-Khomyakov <ser.xomiakoff2012@yandex.ru>
2024-07-27 18:22:43 +0300

    remeve not use code

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,9 +1,4 @@
-import { Section, Cell, Image, List } from '@telegram-apps/telegram-ui';
 import type { FC } from 'react';
-
-import { Link } from '@/components/Link/Link.tsx';
-
-import tonSvg from './ton.svg';
 
 export const IndexPage: FC = () => {
   return (
