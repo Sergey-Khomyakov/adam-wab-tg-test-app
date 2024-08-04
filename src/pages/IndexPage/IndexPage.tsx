b84408d3f27bcfc481f04554ec7c2bdaa6a13c44
Sergey-Khomyakov <ser.xomiakoff2012@yandex.ru>
2024-08-04 15:45:39 +0300

    hidde all btn

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -3,14 +3,6 @@ import { SDKProvider } from '@telegram-apps/sdk-react';
 
 
 export const IndexPage: FC = () => {
-  // const bb = useBackButton(true); // will be undefined or BackButton.
-
-  // useEffect(() => {
-  //   if (bb) {
-  //     // Here we can safely work with the BackButton.
-  //   }
-  // }, [bb]);
-
   return (
     <SDKProvider acceptCustomStyles debug>
       <h1>Telegram Web App v3</h1>
