6c731952af69d284ce8ec5f140a8bbbbbb792e7f
Sergey-Khomyakov <ser.xomiakoff2012@yandex.ru>
2024-08-04 15:47:55 +0300

    commit

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,7 +1,11 @@
 import { FC } from 'react';
 import { SDKProvider } from '@telegram-apps/sdk-react';
 
-
+/**
+ * Renders the IndexPage component.
+ *
+ * @return {JSX.Element} The rendered IndexPage component.
+ */
 export const IndexPage: FC = () => {
   return (
     <SDKProvider acceptCustomStyles debug>
