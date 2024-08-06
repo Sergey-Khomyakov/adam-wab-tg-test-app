a28994f7d1ed7afe5eda9a07ae7981c417d444a7
Sergey-Khomyakov <ser.xomiakoff2012@yandex.ru>
2024-08-07 00:17:31 +0300

    get data

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,14 +1,14 @@
 import { FC } from 'react';
 import { SDKProvider, initInitData } from '@telegram-apps/sdk-react';
 
+const data = initInitData();
 /**
  * Renders the IndexPage component.
  *
  * @return {JSX.Element} The rendered IndexPage component.
  */
+console.log(data);
 export const IndexPage: FC = () => {
-  const data = initInitData();
-  console.log(data);
   return (
     <SDKProvider acceptCustomStyles debug>
       <h1>Telegram Web App v5</h1>
