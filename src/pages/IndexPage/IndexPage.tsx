6e47b723150c0b7ec39090179ed24cccaa18d689
Sergey-Khomyakov <ser.xomiakoff2012@yandex.ru>
2024-08-07 00:37:12 +0300

    get viewPort

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,14 +1,30 @@
-import { FC } from 'react';
-import { SDKProvider, initInitData } from '@telegram-apps/sdk-react';
+import { FC, useEffect } from 'react';
+import {
+  SDKProvider,
+  initInitData,
+  useBackButton,
+  useBackButtonRaw,
+  useViewport,
+  useViewportRaw,
+  useBiometryManagerRaw,
+} from '@telegram-apps/sdk-react';
 
-const data = initInitData();
+// Viewport is being initialized asynchronously, so signal may return undefined.
+// After some time it will receive a valid value.
+const vp = useViewport();
+
+useEffect(() => {
+  console.log(vp); // will be undefined and then Viewport instance.
+}, [vp])
+
 /**
  * Renders the IndexPage component.
  *
  * @return {JSX.Element} The rendered IndexPage component.
  */
+
+
 export const IndexPage: FC = () => {
-  console.dir(data);
   return (
     <SDKProvider acceptCustomStyles debug>
       <h1>Telegram Web App v5</h1>
