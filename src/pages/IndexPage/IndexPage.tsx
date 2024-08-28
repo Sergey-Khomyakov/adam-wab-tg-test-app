126bcf8ee6ae367466116cc488da0cbcee267e03
Sergey-Khomyakov <ser.xomiakoff2012@yandex.ru>
2024-08-28 12:51:42 +0300

    get username

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,28 +1,20 @@
-import { FC, useEffect } from 'react';
+import { FC } from 'react';
 import {
   SDKProvider,
-  useViewport,
+  retrieveLaunchParams,
 } from '@telegram-apps/sdk-react';
-
-// Viewport is being initialized asynchronously, so signal may return undefined.
-// After some time it will receive a valid value.
-const vp = useViewport();
-
-useEffect(() => {
-  console.log(vp); // will be undefined and then Viewport instance.
-}, [vp])
 
 /**
  * Renders the IndexPage component.
  *
  * @return {JSX.Element} The rendered IndexPage component.
  */
-
-
 export const IndexPage: FC = () => {
+  const { initData } = retrieveLaunchParams();
+  const encoding = initData?.user?.username
   return (
     <SDKProvider acceptCustomStyles debug>
-      <h1>Telegram Web App v5</h1>
+      <h1>${encoding}</h1>
     </SDKProvider>
   );
 };
