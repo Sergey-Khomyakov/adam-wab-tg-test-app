1908f03f25daf350399a1f935345b7d8b309e3cf
Sergey-Khomyakov <ser.xomiakoff2012@yandex.ru>
2024-08-30 18:54:18 +0300

    update header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,20 @@
 import { FC } from 'react';
 import classes from './css/Header.module.css'
 import Logo from '@/assets/Logo.svg';
+import LogoWhite from '@/assets/LogoWhite.svg';
 
 import Icons from '@/components/Icons/Icons'
 
 export const Header: FC = () => {
+    const theme = window.matchMedia('(prefers-color-scheme: dark)');
+
+    let logoSrc = theme.matches ? LogoWhite : Logo;
 
     return (
         <div className={classes.header}>
-            <img className={classes.logo} src={Logo} alt="Лого" />
+            <img className={classes.logo} src={logoSrc} alt="Лого" />
             <div className={classes.btn}>
-                <Icons id="Wheelchair"></Icons>
+                <Icons id="List"></Icons>
             </div>
         </div>
     );
